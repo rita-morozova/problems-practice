4d9cf61698ea3f34293dc1201f07266d49a81167
Margarita Morozova <ritamorozova@yahoo.com>
2021-02-05 17:11:43 -0800

    add missing number problem

--- strings/checkTwoStrings.js
+++ strings/checkTwoStrings.js
@@ -1,6 +1,6 @@
-Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+// Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
 
-A string is represented by an array if the array elements concatenated in order forms the string.
+// A string is represented by an array if the array elements concatenated in order forms the string.
 
  
 
