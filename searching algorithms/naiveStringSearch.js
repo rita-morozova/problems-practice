8d53f09b90c95464e564dffd3ec19a0f8f28d917
Margarita Morozova <ritamorozova@yahoo.com>
2021-02-15 17:19:36 -0800

    add naive Search algorithm solution

---
+++ searching algorithms/naiveStringSearch.js
@@ -0,0 +1,19 @@
+// count how many short strings are in the long one
+// naiveSearch('lorie loled', 'lol') => return 1
+
+function naiveSearch(long, short) {
+  let count = 0
+
+  for (let i = 0; i < long.length; i++){
+    for (let j = 0; j < short.length; j++) {
+
+      if(short[j] !== long[i+j]){
+        break
+      }
+      if(j === short.length-1){
+        count ++
+      }
+    }
+  }
+  return count
+}
